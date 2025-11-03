af7571e36917c885761917ffa3a9d13db1326a8e
Daniel Boline <ddboline@gmail.com>
2025-11-02 23:36:03 -0500

    pkg_resources -> importlib

--- src/flask_rq2/__init__.py
+++ src/flask_rq2/__init__.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-    flask_rq2
-    ~~~~~~~~~
+flask_rq2
+~~~~~~~~~
 
-    A Flask extension for RQ (Redis Queue).
+A Flask extension for RQ (Redis Queue).
 
-    :copyright: (c) 2016 by Jannis Leidel.
-    :license: MIT, see LICENSE for more details.
+:copyright: (c) 2016 by Jannis Leidel.
+:license: MIT, see LICENSE for more details.
 """
-from pkg_resources import get_distribution, DistributionNotFound
+
+from importlib.metadata import version, PackageNotFoundError
 
 from .app import RQ  # noqa
 
-__author__ = 'Jannis Leidel'
 
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass
+def get_distribution(pkg_name):
+    try:
+        return version(pkg_name)
+    except PackageNotFoundError:
+        return None
+
+
+__author__ = "Jannis Leidel"
+
+__version__ = get_distribution(__name__)
